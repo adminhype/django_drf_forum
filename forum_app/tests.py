8f4b0beeebe33da86e5fef13de52d19b30bd05d6
Adem <ado.designs@outlook.de>
2025-11-25 13:54:30 +0100

    feat: first test method and test with url

--- forum_app/tests.py
+++ forum_app/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+from django.urls import reverse
+
+
+class LikeTests(APITestCase):
+
+    def test_get_like(self):
+        url = 'http://127.0.0.1:8000/api/forum/likes/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    # def test_get_like(self):
+    #     url = reverse('like-list')
+    #     response = self.client.get(url)
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    # def test_get_likes(self):
+    #     url = reverse('like-list')
+    #     response = self.client.get(url)
+    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)

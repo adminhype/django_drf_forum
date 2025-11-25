698a3dc3ee1204da6add943e3382d4ca67aa1c19
Adem <ado.designs@outlook.de>
2025-11-25 21:56:15 +0100

    feat: test with DetailView and setUp

--- forum_app/tests.py
+++ forum_app/tests.py
@@ -1,7 +1,13 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
 
+
 from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+from forum_app.api.serializers import QuestionSerializer
+from forum_app.models import Question
 
 
 class LikeTests(APITestCase):
@@ -11,12 +17,19 @@ class LikeTests(APITestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    # def test_get_like(self):
-    #     url = reverse('like-list')
-    #     response = self.client.get(url)
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    # def test_get_likes(self):
-    #     url = reverse('like-list')
-    #     response = self.client.get(url)
-    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+class QuestionTests(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', password='testpass')
+        self.question = Question.objects.create(
+            title='Test Question', content='test content', author=self.user, category='frontend')
+
+    def test_detail_question(self):
+        url = reverse('question-detail', kwargs={'pk': self.question.id})
+        response = self.client.get(url)
+        expected_data = QuestionSerializer(self.question).data
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, expected_data)
